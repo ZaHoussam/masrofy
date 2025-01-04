68773d2e97f417a6b1cde9bb0395fcc5b727657e
ZaHoussam <mrlhou62@gmail.com>
2025-01-04 23:47:24 +0100

    Upload: Design

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,4 +1,4 @@
-export { default as Home } from "./Home.page";
+export { default as Home } from "./Home/Home.page";
 export { default as AddTransaction } from "./AddTransaction.page";
 export { default as VisualReports } from "./VisualReports.page";
 export { default as TransactionList } from "./TransactionList.page";
