f254b2678964b54ed9805004442d91b96292c085
ZaHoussam <mrlhou62@gmail.com>
2025-01-04 23:46:19 +0100

    Upload: Design

--- src/Pages/Home/Home.page.jsx
+++ src/Pages/Home/Home.page.jsx
@@ -1,11 +1,13 @@
+import style from "./Home.module.css";
+
 const Home = () => {
   return (
-    <div>
+    <section className={style.home}>
       <h1>Welcome to Masroofy</h1>
       <p>Total Income: DZD 0</p>
       <p>Total Expenses: DZD 0</p>
       <p>Balance: DZD 0</p>
-    </div>
+    </section>
   );
 };
 export default Home;
